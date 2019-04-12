fc66254412642ed552b920de83d1b094147cd529
LeonHuang <hht51770@hotmail.com>
2019-04-13 00:44:53 +0800

    Updates

--- precache-manifest.fab4a966f8b6587b8c9292c87829e051.js
+++ precache-manifest.fab4a966f8b6587b8c9292c87829e051.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-blog-share-code/static/js/runtime~main.c38112b0.js"
   },
   {
-    "revision": "942369c1718d4a30e9a2",
-    "url": "/react-blog-share-code/static/js/main.a8ab7b6b.chunk.js"
+    "revision": "a28fccccbb673fd4c3dc",
+    "url": "/react-blog-share-code/static/js/main.3de8adaa.chunk.js"
   },
   {
     "revision": "a77eef4331c2e0cbcc05",
     "url": "/react-blog-share-code/static/js/2.5a9dcbfd.chunk.js"
   },
   {
-    "revision": "942369c1718d4a30e9a2",
+    "revision": "a28fccccbb673fd4c3dc",
     "url": "/react-blog-share-code/static/css/main.a211195e.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-blog-share-code/static/css/2.70080bc8.chunk.css"
   },
   {
-    "revision": "eea9655d63ab5ffca70dd0af6844742b",
+    "revision": "6fcb08ef1e208d61ee84be6bf24effe6",
     "url": "/react-blog-share-code/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-blog-share-code/precache-manifest.9da3052f1f0f5911f49eaf660736025b.js"
+  "/react-blog-share-code/precache-manifest.fab4a966f8b6587b8c9292c87829e051.js"
 );
 
 workbox.clientsClaim();
